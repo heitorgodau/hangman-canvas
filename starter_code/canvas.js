befacd3f9d60c99b5f4f15881d52406d4ba32817
Heitor <heitorgodau@gmail.com>
2019-08-31 10:09:10 -0300

    missing some little adjusts in canvas part

---
+++ starter_code/canvas.js
@@ -0,0 +1,106 @@
+
+class HangmanCanvas {
+  constructor(secretWord) {
+    this.ctx = document.getElementById('hangman').getContext('2d');
+    this.secretWord = secretWord;
+  }
+  createBoard() {
+    this.ctx.clearRect(0, 0, 1200, 800)
+    this.ctx.beginPath();
+    this.ctx.moveTo(150, 600);
+    this.ctx.lineTo(0, 700);
+    this.ctx.lineTo(300, 700);
+    this.ctx.lineTo(150, 600);
+    this.ctx.lineTo(150, 200);
+    this.ctx.lineTo(450, 200);
+    this.ctx.lineTo(450, 300);
+    this.ctx.stroke();
+  }
+
+  drawLines() {
+    let x = 350;
+    const y = 700; 
+    for (let i = 0; i < this.secretWord.length; i += 1) {
+      this.ctx.moveTo(x,y);
+      this.ctx.lineTo(x + 60, y);
+      this.ctx.stroke();
+      x += 70;
+    }
+  }
+  
+  writeCorrectLetter(idx) {
+    this.ctx.font = '70px Georgia';
+    const letter = this.secretWord[idx]
+    this.ctx.fillText(letter.toUpperCase(), 350 + (idx * 70), 695);
+  }
+
+  writeWrongLetter(key) {
+    const randomX = Math.floor(Math.random() * (1000 - 700)) + 700 
+    const randomY = Math.floor(Math.random() * (400 - 50)) + 50 
+    this.ctx.font = '70px Georgia'
+    this.ctx.fillText(key, randomX, randomY);    
+  }
+
+  drawHangman(shape) {
+    this.ctx.closePath();
+    this.ctx.beginPath();
+    switch (shape) {
+      case 'head':
+        this.ctx.beginPath();
+        this.ctx.arc(450, 330, 30, 0, 7);
+        this.ctx.closePath();
+        break;
+      case 'body':
+        this.ctx.beginPath();
+        this.ctx.moveTo(450, 360);
+        this.ctx.lineTo(450,450);
+        this.ctx.stroke();
+        this.ctx.closePath();
+        break;
+      case 'left-arm':
+        this.ctx.beginPath();
+        this.ctx.moveTo(450, 370);
+        this.ctx.lineTo(400, 410);
+        this.ctx.stroke();
+        this.ctx.closePath();
+        break;
+      case 'right-arm':
+        this.ctx.beginPath();
+        this.ctx.moveTo(450, 370);
+        this.ctx.lineTo(500, 410);
+        this.ctx.stroke();
+        this.ctx.closePath();
+        break;
+      case 'left-leg':
+        this.ctx.beginPath();
+        this.ctx.moveTo(500, 405);
+        this.ctx.lineTo(400, 505);
+        this.ctx.stroke();
+        this.ctx.closePath();
+        break;
+      case 'rigth-leg':
+        this.ctx.beginPath();
+        this.ctx.moveTo(500, 405);
+        this.ctx.lineTo(600, 505);
+        this.ctx.stroke();
+        this.ctx.closePath();
+        break;
+    }
+  }
+
+  gameOver() {
+    let img = document.getElementById("loser");
+    this.ctx.drawImage(img, 10, 10);
+    setTimeout(() => {
+      alert('Your soul is mine! :] ');
+    }, 100);
+  }
+
+  winner() {
+    let img = document.getElementById("winner");
+    this.ctx.drawImage(img, 10, 10);
+    setTimeout(() => {
+      alert('I don\'t wanna your filthy soul. Go to heaven! >: ');
+    }, 100);
+  }
+}
---
+++ starter_code/hangman.js
@@ -0,0 +1,98 @@
+class Hangman {
+  constructor(){
+    this.words = ['SATAN', 'BEELZEBUB', 'BELIAL', 'LUCIFER'];
+    this.secretWord = this.getWord();
+    this.letters = [];
+    this.guessedLetter = '';
+    this.errorsLeft = 10;
+  }
+
+  getWord() {
+    const randomIdx = Math.floor(Math.random() * this.words.length);
+    return this.secretWord = this.words[randomIdx].toUpperCase();
+  }
+
+  checkIfLetter(keyCode) {
+    return (keyCode >= 65 && keyCode <= 90)
+  }
+
+  checkClickedLetters(key) {
+    if (this.letters.includes(key)) {
+      return false;
+    }
+    if (this.secretWord.includes(key)) {
+      let index = this.secretWord.indexOf(key);
+      while(this.secretWord.indexOf(key, index) !== -1) {
+        let pos = this.secretWord.indexOf(key, index);
+        this.addCorrectLetter(pos);
+        hangmanCanvas.writeCorrectLetter(pos);
+        index = pos + 1;
+      }
+      this.checkWinner();
+      return true;
+    } else if (!this.letters.includes(key) && !this.secretWord.includes(key)) {
+      this.addWrongLetter(key, this.errorsLeft);
+      hangmanCanvas.writeWrongLetter(key);
+      this.checkGameOver();
+    }
+    return false;
+  }
+
+  checkGameOver() {
+    if (this.errorsLeft <= 1) {
+      hangmanCanvas.gameOver();
+      return true;
+    }
+    return false;
+  }
+
+  checkWinner() {
+    const sortWord = (word) => [...word].sort().join(''); 
+    if (sortWord(this.secretWord) === sortWord(this.guessedLetter)) {
+      hangmanCanvas.winner();
+      return true;
+    }
+    return false;
+  }
+
+  addCorrectLetter(i) {
+    this.guessedLetter += this.secretWord[i].toUpperCase();
+  }
+
+  addWrongLetter(letterArg, errorsLeft) {
+    this.letters.push(letterArg);
+    this.errorsLeft -= 1;
+    switch (errorsLeft) {
+      case 8:
+        hangmanCanvas.drawHangman('head');
+        break;
+      case 6:
+        hangmanCanvas.drawHangman('left-arm');
+        break;
+      case 4:
+        hangmanCanvas.drawHangman('right-arm');
+        break;
+      case 2:
+        hangmanCanvas.drawHangman('legs');
+        break;
+    }
+  }
+}
+let hangman;
+let hangmanCanvas;
+
+document.getElementById('start-game-button').onclick = function () {
+  hangman = new Hangman();
+  hangmanCanvas = new HangmanCanvas(hangman.secretWord);
+  hangmanCanvas.createBoard();
+  hangmanCanvas.drawLines();
+};
+
+
+document.onkeydown = function (e) {
+  const key = e.key.toUpperCase();
+  const keyCode = e.keyCode;
+  if (hangman.checkIfLetter(keyCode)) {
+    hangman.checkClickedLetters(key)
+  }
+};
